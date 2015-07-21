16de5e146ad7ac2510b77e2d3f7b8b01e9272d65
moizjv <moizjv@gmail.com>
2015-07-21 00:06:53 -0700

    Adding more unit tests

--- test/unit/bootstrap-specs.js
+++ test/unit/bootstrap-specs.js
@@ -27,7 +27,7 @@ describe('AndroidBootstrap', function () {
             disableAndroidWatchers = false;
       androidBootstrap.adb = adb;
       androidBootstrap.uiAutomator = uiAutomator;
-      S.mocks.androidBootstrap.expects('createUiAutomator').once()
+      S.mocks.androidBootstrap.expects('init').once()
         .returns('');
       S.mocks.adb.expects('forwardPort').once()
         .withExactArgs(systemPort, systemPort)
@@ -37,6 +37,61 @@ describe('AndroidBootstrap', function () {
         .returns(conn);
       S.mocks.net.expects('connect').once().returns(conn);
       await androidBootstrap.start(appPackage, disableAndroidWatchers);
+      S.verify();
+    });
+  }));
+  describe("sendCommand", () => {
+    it("should timeout", async function () {
+      let conn = new events.EventEmitter();
+      conn.write = () => {};
+      conn.setEncoding = () => {};
+      androidBootstrap.socketClient = conn;
+      await androidBootstrap.sendCommand('action', {action: 'getDataDir'}, 1000)
+        .should.eventually.be.rejectedWith("Bootstrap");
+    });
+    it("should successfully return after receiving data from bootstrap in parts", async function () {
+      let conn = new events.EventEmitter();
+      conn.write = () => {};
+      conn.setEncoding = () => {};
+      androidBootstrap.socketClient = conn;
+      setTimeout(() => {
+        conn.emit("data", '{"status":0, ');
+        conn.emit("data", '"value": "hello"}');
+      }, 0);
+      await androidBootstrap.sendCommand('action', {action: 'getDataDir'}, 1000);
+    });
+    it("should successfully return after receiving data from bootstrap", async function () {
+      let conn = new events.EventEmitter();
+      conn.write = () => {};
+      conn.setEncoding = () => {};
+      androidBootstrap.socketClient = conn;
+      setTimeout(() => {
+        conn.emit("data", '{"status":0, "value": "hello"}');
+      }, 0);
+      await androidBootstrap.sendCommand('action', {action: 'getDataDir'}, 1000);
+    });
+  });
+  describe("sendAction", withSandbox({mocks: {androidBootstrap}}, (S) => {
+    it("should call sendCommand", async function () {
+      let extra = {action: 'wake', params: {}};
+      S.mocks.androidBootstrap.expects('sendCommand').once()
+        .withExactArgs('action', extra)
+        .returns('');
+      await androidBootstrap.sendAction('wake');
+      S.verify();
+    });
+  }));
+  describe("shutdown", withSandbox({mocks: {androidBootstrap, uiAutomator}}, (S) => {
+    it("should call sendCommand", async function () {
+      let conn = new events.EventEmitter();
+      androidBootstrap.socketClient = conn;
+      S.mocks.androidBootstrap.expects('sendCommand').once()
+        .withExactArgs('shutdown')
+        .returns('');
+      S.mocks.uiAutomator.expects("shutdown")
+        .once()
+        .returns("");
+      await androidBootstrap.shutdown();
       S.verify();
     });
   }));
