faa5ca382626b29ef3ac95bb746d2582ff7d717e
Jonah Stiennon <jonahss@gmail.com>
2015-08-07 17:01:38 -0700

    build bootstrap.jar using maven when publishing.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,21 @@
 "use strict";
+var teen_process = require('teen_process');
 
 var gulp = require('gulp'),
     boilerplate = require('appium-gulp-plugins').boilerplate.use(gulp);
 
-boilerplate({build: 'appium-android-bootstrap', jscs: false});
+gulp.task('maven-clean', function () {
+  return teen_process.exec('mvn', ['clean'], {cwd: 'bootstrap'});
+});
+
+gulp.task('maven-install', ['maven-clean'], function () {
+  return teen_process.exec('mvn', ['install'], {cwd: 'bootstrap'});
+});
+
+gulp.task('maven', ['maven-clean', 'maven-install']);
+
+boilerplate({
+  build: 'appium-android-bootstrap',
+  jscs: false,
+  extraPrepublishTasks: ['maven']
+});
