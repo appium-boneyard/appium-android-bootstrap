f8fc9f3ec19f437b3f0770d32fb19e44f93019e8
Jonah Stiennon <jonahss@gmail.com>
2015-08-10 11:53:09 -0700

    switch from maven to ant build. ant properly builds the android dex files

--- gulpfile.js
+++ gulpfile.js
@@ -4,18 +4,18 @@ var teen_process = require('teen_process');
 var gulp = require('gulp'),
     boilerplate = require('appium-gulp-plugins').boilerplate.use(gulp);
 
-gulp.task('maven-clean', function () {
-  return teen_process.exec('mvn', ['clean'], {cwd: 'bootstrap'});
+gulp.task('ant-clean', function () {
+  return teen_process.exec('ant', ['clean'], {cwd: 'bootstrap'});
 });
 
-gulp.task('maven-install', ['maven-clean'], function () {
-  return teen_process.exec('mvn', ['install'], {cwd: 'bootstrap'});
+gulp.task('ant-build', ['ant-clean'], function () {
+  return teen_process.exec('ant', ['build'], {cwd: 'bootstrap'});
 });
 
-gulp.task('maven', ['maven-clean', 'maven-install']);
+gulp.task('ant', ['ant-clean', 'ant-build']);
 
 boilerplate({
   build: 'appium-android-bootstrap',
   jscs: false,
-  extraPrepublishTasks: ['maven']
+  extraPrepublishTasks: ['ant']
 });
