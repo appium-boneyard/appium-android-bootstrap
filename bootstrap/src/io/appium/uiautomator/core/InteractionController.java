2d20e71fe8f0607a3ff6aa274c1e6373a4cc8cb4
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2016-08-13 22:42:39 -0300

    Fix https://github.com/appium/appium-android-driver/issues/171
    Support touch actions starting and ending at different moments

--- bootstrap/src/io/appium/uiautomator/core/InteractionController.java
+++ bootstrap/src/io/appium/uiautomator/core/InteractionController.java
@@ -16,8 +16,6 @@
 package io.appium.uiautomator.core;
 
 import android.view.InputEvent;
-import android.view.MotionEvent;
-import android.view.MotionEvent.PointerCoords;
 
 import static io.appium.android.bootstrap.utils.ReflectionUtils.invoke;
 import static io.appium.android.bootstrap.utils.ReflectionUtils.method;
@@ -30,7 +28,6 @@ public class InteractionController {
   private static final String METHOD_TOUCH_DOWN = "touchDown";
   private static final String METHOD_TOUCH_UP = "touchUp";
   private static final String METHOD_TOUCH_MOVE = "touchMove";
-  public static final String METHOD_PERFORM_MULTI_POINTER_GESTURE = "performMultiPointerGesture";
 
   private final Object interactionController;
 
@@ -56,9 +53,5 @@ public class InteractionController {
 
   public boolean touchMove(int x, int y) {
     return (Boolean) invoke(method(CLASS_INTERACTION_CONTROLLER, METHOD_TOUCH_MOVE, int.class, int.class), interactionController, x, y);
-  }
-
-  public Boolean performMultiPointerGesture(MotionEvent.PointerCoords[][] pcs) {
-    return (Boolean) invoke(method(CLASS_INTERACTION_CONTROLLER, METHOD_PERFORM_MULTI_POINTER_GESTURE, PointerCoords[][].class), interactionController, (Object) pcs);
   }
 }
